93eeaa38df24ad21db6bfb091f19ca48d919eb2a
karyna.avetsisian <avetisyan.k.workmail@gmail.com>
2017-11-22 18:29:15 +0300

    commit

--- Intersection.js
+++ Intersection.js
@@ -6,11 +6,37 @@
  * @param  {number[]} right
  * @return {number[]}
  */
-function intersection(left, right) {
+function intersection(leftArray, rightArray) {
+    if(isValid(leftArray) || isValid(rightArray)){
+        return null;
+    }
 
+    let lengthLeftArray = leftArray.length;
+    let sameElementsArray = [];
+
+    for (let index = 0; index < lengthLeftArray; index++) {
+        if (isContain(leftArray[index], rightArray)) {
+            sameElementsArray.push(leftArray[index]);
+        }
+    }
+
+    return sameElementsArray;
 }
 
 console.log(intersection(
     [1, 2, 3, 4, 5],
     [2, 8, 3]
 )); //должно вернуть [2, 3]
+//
+console.log(intersection(
+    [1, 2, 3, 4, 5]
+)); //должно вернуть [2, 3]
+
+function isContain(value, arrayOfValues) {
+    return arrayOfValues.indexOf(value) !== -1;
+}
+
+function isValid(value) {
+    let lengthOfValue = value.length;
+    return value || Array.isArray(value) || lengthOfValue;
+}
--- Missing.js
+++ Missing.js
@@ -5,10 +5,94 @@
  * @param  {number[]} values
  * @return {boolean}
  */
-function missing(values) {
 
+// function missing(arrayOfValues) {
+//     // let maxValue = Math.max.apply(null, arrayOfValues);
+//     let maxValue = arrayOfValues.reduce( (prevValue, currentValue) => {
+//         return Math.max(prevValue, currentValue);
+//     });
+//     let lengthOfValues = arrayOfValues.length;
+//
+//     if( maxValue !== lengthOfValues ){
+//         let sumOfValues = arrayOfValues.reduce((resultSum, currentValue) =>{
+//             return resultSum += currentValue;
+//         });
+//         let checkSum = (1 + maxValue)*maxValue/2;
+//
+//         let missingNumber = checkSum - sumOfValues;
+//
+//         return missingNumber;
+//     }
+// }
+
+function missing(arrayOfValues) {
+
+    if(!isValid(arrayOfValues)){
+        return null;
+    }
+
+    let maxValue = arrayOfValues.reduce((prevValue, currentValue) => {
+        return Math.max(prevValue, currentValue);
+    });
+    let lengthOfValues = arrayOfValues.length;
+
+    if (maxValue !== lengthOfValues) {
+        for (let number = 1; number <= maxValue; number++) {
+          if(!isContains(number, arrayOfValues)){
+              return number;
+          }
+        }
+    }
 }
 
-console.log(missing([1, 4, 3])); // 2
+// /*если считать от произвольного числа */
+// function missing(arrayOfValues) {
+//
+//     if(!isValid(arrayOfValues)){
+//         return null;
+//     }
+//
+//     let maxValue = arrayOfValues.reduce((prevValue, currentValue) => {
+//         return Math.max(prevValue, currentValue);
+//     });
+//     let minValue = arrayOfValues.reduce((prevValue, currentValue) => {
+//         return Math.min(prevValue, currentValue);
+//     });
+//     let lengthOfValues = arrayOfValues.length;
+//
+//     if (maxValue !== lengthOfValues) {
+//         for (let index = minValue; index <= maxValue; index++) {
+//             console.log(isContains(index, arrayOfValues) );
+//             if(!isContains(index, arrayOfValues)){
+//                 return index;
+//             }
+//         }
+//     }
+// }
+
+console.log(missing([1, 3, 4])); // 2
 console.log(missing([5, 1, 4, 2])); // 3
 console.log(missing([1, 2, 3, 4])); // undefined
+
+function isContains(value, arrayOfValues) {
+    let returnIsContains = false;
+
+    if(arrayOfValues.indexOf(value) !== -1){
+        returnIsContains = true;
+    }
+
+    return returnIsContains;
+}
+
+function isValid(arrayOfValues) {
+    let lengthOfValue = arrayOfValues.length;
+
+    return arrayOfValues && Array.isArray(arrayOfValues) && lengthOfValue && isArrayOfNumber(arrayOfValues);
+}
+
+function isArrayOfNumber(arrayOfValues) {
+
+    return arrayOfValues.every( (element) => {
+        return typeof element === 'number';
+    });
+}
