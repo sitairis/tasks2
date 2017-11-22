82ae5d1c3b3b0ead23292bd39aa006539c475a7b
karyna.avetsisian <avetisyan.k.workmail@gmail.com>
2017-11-22 15:16:31 +0300

    Initial commit

---
+++ Abbreviation.js
@@ -0,0 +1,17 @@
+// 3. Аббревиатура штатов США (необязательно через массивы, freestyle задача)
+
+/**
+ * Вовзращает аббревиатуру штата США.
+ *
+ * @param {string} state - название штата (на английском, только из США). Может прийти из любого источника,
+ *  в любом виде (правильное/неправильное название, в lowerCase/upperCase, может прийти готовая аббревиатура и т.д.)
+ *
+ * @return {string} - аббревиатура штата (в upper case)
+ */
+function getAbbreviation(state) {
+
+}
+
+getAbbreviation('Florida'); // 'FL'
+getAbbreviation('FL'); // 'FL'
+getAbbreviation('KDJFDLFJ'); // 'undefined'
---
+++ Intersection.js
@@ -0,0 +1,16 @@
+// 1. Пересечения
+
+/**
+ * Найти пересечение двух массивов
+ * @param  {number[]} left
+ * @param  {number[]} right
+ * @return {number[]}
+ */
+function intersection(left, right) {
+
+}
+
+console.log(intersection(
+    [1, 2, 3, 4, 5],
+    [2, 8, 3]
+)); //должно вернуть [2, 3]
